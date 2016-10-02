8daffcf68a4bbbbff76d543b442ea24b68ca387a
arpchauh <arpitchauhan10@gmail.com>
2016-10-02 09:44:15 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.acadgild.java.session4;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//B o1 = new B();
+		Parent p = new Child();
+		p.method2();
+		//A ob1 = new A();
+	}
+
+}
