9b02dbccc05c520028f98ccae6c603e080160666
arpchauh <arpitchauhan10@gmail.com>
2016-10-02 09:43:23 +0530

    Update Parent.java

--- Parent.java
+++ Parent.java
@@ -1,5 +1,20 @@
 package com.acadgild.java.session4;
 
-public class Parent {
-
+class Parent{
+private void method1(){
+ System.out.println ("Parent's method1()");
 }
+public void method2() {
+ System.out.println ("Parent's method2()");
+method1();
+}
+}
+ class Child extends Parent {
+ public void method1() {
+ System.out.println ("Child's method1()");
+} 
+ }
+
+/* output -  Parent's method2()
+             Parent's method1()*/
+ 
